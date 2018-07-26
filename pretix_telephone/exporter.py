b9922446b6cfb54e92c2d01f8181cf7e18f14a26
Felix Rindt <felix@rindt.me>
2018-07-26 14:59:44 +0200

    change to csv

--- pretix_telephone/exporter.py
+++ pretix_telephone/exporter.py
@@ -1,5 +1,7 @@
+import io
 import json
 from collections import OrderedDict
+from defusedcsv import csv
 
 from django import forms
 from django.utils.translation import ugettext_lazy as _
@@ -9,16 +11,32 @@ from pretix.base.models import Order
 
 class TelephoneExporter(BaseExporter):
     identifier = 'telephonenumbers'
-    verbose_name = _('Telephone numbers (text file)')
+    verbose_name = _('Telephone numbers (CSV)')
 
     def render(self, form_data: dict):
-        numbers = []
-        for ordervalues in self.event.orders.filter(status__in=form_data['status']).values('meta_info'):
-            contact_form_data = json.loads(ordervalues['meta_info'])['contact_form_data']
+        output = io.StringIO()
+        if form_data.get('dialect', '-') in csv.list_dialects():
+            writer = csv.writer(output, dialect=form_data.get('dialect'))
+        else:
+            writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC, delimiter=",")
+
+        writer.writerow([_('Order code'), _('Name'), _('Telephone')])
+
+        for order in self.event.orders.filter(status__in=form_data['status']):
+            row = [order.code]
+            try:
+                row.append(order.invoice_address.name)
+            except InvoiceAddress.DoesNotExist:
+                row.append("")
+            contact_form_data = json.loads(order.meta_info)['contact_form_data']
             if 'telephone' in contact_form_data:
-                numbers.append(contact_form_data['telephone'])
-        data = "\r\n".join(numbers)
-        return '{}_pretixtelephonenumbers.txt'.format(self.event.slug), 'text/plain', data.encode("utf-8")
+                row.append(contact_form_data['telephone'])
+            else:
+                row.append("")
+            writer.writerow(row)
+
+        return '{}_telephone.csv'.format(self.event.slug), 'text/csv', output.getvalue().encode("utf-8")
+
 
     @property
     def export_form_fields(self):
@@ -32,6 +50,11 @@ class TelephoneExporter(BaseExporter):
                      widget=forms.CheckboxSelectMultiple,
                      required=False
                  )),
+                ('dialect',
+                 forms.ChoiceField(
+                    label=_('CSV dialect'),
+                    choices=(('default', 'Default'), ('excel', 'Excel'))
+                )),
             ]
         )
 
